89cc611346144bc1c095e6ebaff10b5a63bd57cf
Porson <porsonlee@gmail.com>
2019-12-04 20:56:30 +0000

    added play.js

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.88.149',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
