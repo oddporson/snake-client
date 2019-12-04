f7ce514bc0064fc16044e4930f01cc46c7615ef9
Porson <porsonlee@gmail.com>
2019-12-04 23:43:03 +0000

    added setupInput for module.exports

--- play.js
+++ play.js
@@ -1,6 +1,15 @@
-const { connect } = require('./client'); // in client.js, module.export = { connect }. This is object destructuring.
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
+
+setupInput();
+
+// const { setUpInput } = require('./input')
+// const { connect } = require('./client'); // in client.js, module.export = { connect }. This is object destructuring.
+// console.log('Connecting ...');
+// connect();
 
 // This option also works:
 // const connect = require('./client'); // in client.js, module.export = connnect
@@ -11,19 +20,4 @@ connect();
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on('data', handleUserInput);
-  
-  return stdin;
-}
-setupInput()
