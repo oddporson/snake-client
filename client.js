e9031fc51f51f729093ce52bf0a63028545e3d19
Porson <porsonlee@gmail.com>
2019-12-04 21:52:25 +0000

    added name command

--- client.js
+++ client.js
@@ -11,6 +11,11 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server.')
+    conn.write('Name: PHL')
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
