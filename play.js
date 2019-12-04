7d5d072dc08ae81cc8631ac61f7ec736313c7d13
Porson <porsonlee@gmail.com>
2019-12-04 23:25:40 +0000

    created function to ctrl-c to terminate out of the terminal with @AliceMathews

--- play.js
+++ play.js
@@ -6,3 +6,25 @@ connect();
 // const connect = require('./client'); // in client.js, module.export = connnect
 // console.log('Connecting ...');
 // connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput);
+  
+  return stdin;
+}
+
+setupInput()
