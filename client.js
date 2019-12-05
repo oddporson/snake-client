44983d0a9743526725aacd79c1ce976d8a66a248
Porson <porsonlee@gmail.com>
2019-12-05 01:03:03 +0000

    added IP and PORT as const variable for module.export purpose

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: '192.168.88.149',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -14,15 +14,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server.')
     conn.write('Name: PHL')
-
-    // setInterval(() => {
-    //   conn.write('Move": up')}, 500);
-    //   setInterval(() => {
-    //     conn.write('Move": down')}, 500);
-    //     setInterval(() => {
-    //       conn.write('Move": right')}, 500);
-    //       setInterval(() => {
-    //         conn.write('Move": left')}, 500);
   });
 
   
