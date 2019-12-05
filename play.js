dc7e7c9803e311e23b885b0cf8bd090ec4408bd2
Porson <porsonlee@gmail.com>
2019-12-05 00:39:34 +0000

    re-tweak the connection

--- play.js
+++ play.js
@@ -2,9 +2,8 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
-
-setupInput();
+const conn = connect();
+setupInput(conn);
 
 // This option also works:
 // const connect = require('./client'); // in client.js, module.export = connnect
