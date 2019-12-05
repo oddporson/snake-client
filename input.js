1d8cf2dc2974b55446711be579f16ff0249b983a
Porson <porsonlee@gmail.com>
2019-12-05 00:38:58 +0000

    changed in if key from conn to connection

--- input.js
+++ input.js
@@ -1,17 +1,37 @@
-const setupInput = function() {
+let connection; 
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-
+  
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     }
+  
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+  
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+  
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+  
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
   }
+  
   stdin.on('data', handleUserInput);
   
   return stdin;
 }
+
 
 module.exports = { setupInput }
